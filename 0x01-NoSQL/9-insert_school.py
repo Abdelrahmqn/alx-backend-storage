e9184e513c267e727db628eb567d37007a5b5926
Abdelrahman <boda8099@gmail.com>
2024-08-18 17:19:03 +0300

    all

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""insert many"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert a coll of documents."""
+    insertion = mongo_collection.insert_one(kwargs)
+    return insertion.inserted_id
