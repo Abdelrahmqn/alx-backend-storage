7c8a395465a20407e3a9560c373322db0bfbd9ee
Abdelrahman <boda8099@gmail.com>
2024-08-18 18:17:02 +0300

    is list iterates over the school?

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""schools_by_topic"""
+
+
 def schools_by_topic(mongo_collection, topic):
+    """iterates over the school based on the topic"""
     return list(mongo_collection.find({"topics": topic}))
